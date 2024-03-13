7b6ed2f3731c4ebad5bcd1b97ccac3503305d73a
Yaksh <yakshbha@gmail.com>
2024-03-13 14:57:58 -0400

    Initial Commit

---
+++ TSA_NJtourism/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TSA_NJtourism
+//
+//  Created by Yaksh Bhatia on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TSA_NJtourism/TSA_NJtourismApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSA_NJtourismApp.swift
+//  TSA_NJtourism
+//
+//  Created by Yaksh Bhatia on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TSA_NJtourismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
