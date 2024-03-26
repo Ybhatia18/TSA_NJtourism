cc337ca1b9d3414fb547f84428395d1f9e96485d
Racecarboy <dkorracecarboy@gmail.com>
2024-03-25 22:38:53 -0400

    jjj

--- TSA_NJtourism/ContentView.swift
+++ TSA_NJtourism/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("NJhhh")
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            Button("Buttonhhh") {
                 /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
             }
         }
