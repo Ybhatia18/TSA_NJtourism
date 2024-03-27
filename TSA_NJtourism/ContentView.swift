5fabf6a520fa0ef65964fc62436f96841b5e2256
Yaksh <yakshbha@gmail.com>
2024-03-26 21:03:20 -0400

    Added the spots list

--- TSA_NJtourism/ContentView.swift
+++ TSA_NJtourism/ContentView.swift
@@ -3,7 +3,22 @@
 //  TSA_NJtourism
 //
 //  Created by Yaksh Bhatia on 3/13/24.
-//
+/*
+ Spots to integrate:
+ > Duke Farms
+ > Columbus Park
+ > Hoboken Shoreline
+ > dWashington Rock
+ > Albert Einsteins
+ > American Dream Mall
+ > Six Flags
+ > Atlantic City
+ > Supercharged
+ > Liberty Science Center
+ > Trenton Capital Building
+ */
+
+
 
 import SwiftUI
 import MapKit
